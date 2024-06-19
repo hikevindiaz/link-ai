0551f0ca901e6e7fbfa23d04742965a6c2ec123c
hikevindiaz <143301224+hikevindiaz@users.noreply.github.com>
2024-06-19 11:32:58 -0400

    Update site.ts

--- config/site.ts
+++ config/site.ts
@@ -1,14 +1,14 @@
 import { SiteConfig } from "@/types"
 
 export const siteConfig: SiteConfig = {
-  name: "OpenAssistantGPT",
+  name: "Link",
   description:
-    "An Open-Source SaaS Platform for Crafting Chatbots with OpenAI's Assistant.",
-  url: "https://www.openassistantgpt.io/",
+    "Use Link to convert clients way faster.",
+  url: "https://www.trudigital.agency/link",
   ogImage: "https://www.openassistantgpt.io/dashboard.png",
   links: {
     twitter: "https://twitter.com/oassistantgpt",
-    github: "https://github.com/marcolivierbouch/OpenAssistantGPT",
-    productHunt: "https://www.producthunt.com/posts/openassistantgpt",
+    github: "https://www.openassistantgpt.io/dashboard.png",
+    productHunt: "https://www.openassistantgpt.io/dashboard.png",
   },
 }
