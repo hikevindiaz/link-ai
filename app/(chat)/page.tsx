6acf624267368d4cb903db335a24fd22bb6fe4f1
Kevin Diaz <kevindiaz@Kevins-MBP.lan>
2025-03-21 09:05:24 -0400

    Fix generateUUID reference in chat page

--- app/(chat)/page.tsx
+++ app/(chat)/page.tsx
@@ -2,7 +2,7 @@ import { cookies } from 'next/headers';
 
 import { Chat } from '@/components/chat-interface/chat';
 import { DEFAULT_CHAT_MODEL } from '@/lib/chat-interface/ai/models';
-import { generateId } from '@/lib/chat-interface/utils';
+import { generateId, generateUUID } from '@/lib/chat-interface/utils';
 import { DataStreamHandler } from '@/components/chat-interface/data-stream-handler';
 
 export default async function Page() {
@@ -17,6 +17,7 @@ export default async function Page() {
         <Chat
           key={id}
           id={id}
+          chatbotId=""
           initialMessages={[]}
           selectedChatModel={DEFAULT_CHAT_MODEL}
           selectedVisibilityType="private"
@@ -32,6 +33,7 @@ export default async function Page() {
       <Chat
         key={id}
         id={id}
+        chatbotId=""
         initialMessages={[]}
         selectedChatModel={modelIdFromCookie.value}
         selectedVisibilityType="private"
