cc43453bd5e988a8c5a7262cf268152b568a8512
Kevin Diaz <kevindiaz@Kevins-MBP.lan>
2025-03-24 18:35:06 -0400

    Add Progress component with Radix UI integration

--- components/ui/progress.tsx
+++ components/ui/progress.tsx
@@ -18,7 +18,7 @@ const Progress = React.forwardRef<
     {...props}
   >
     <ProgressPrimitive.Indicator
-      className="h-full w-full flex-1 bg-blue-500 transition-all dark:bg-blue-500"
+      className="h-full w-full flex-1 bg-indigo-500 transition-all dark:bg-indigo-500"
       style={{ transform: `translateX(-${100 - (value || 0)}%)` }}
     />
   </ProgressPrimitive.Root>
